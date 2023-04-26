b2ee7cb21d150961e2c6f59be43733c6099ae092
Rahul Pal <104918276+rahul4587@users.noreply.github.com>
2023-04-26 14:35:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function display(v){
+    document.getElementById("result").value+=v;
+ }
+ function del(){
+   document.getElementById("result").value= document.getElementById("result").value.slice(0,-1);
+ }
+ function clearScreen(){
+   document.getElementById("result").value="";
+ }
+ function sqrt(){
+    var a = document.getElementById("result").value;
+    var b= Math.sqrt(a);
+    document.getElementById("result").value=b;
+ }
+ function calculate(){
+    var x= document.getElementById("result").value;
+    var y= eval(x);
+    document.getElementById("result").value=y;
+    
+ }
